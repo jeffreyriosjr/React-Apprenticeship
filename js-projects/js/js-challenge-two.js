5f7f1a7b54a6800f2760cbf0fcf30f2a9678fbba
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-02-23 22:56:50 -0800

    attempt to complete js-challenge-two

---
+++ js-projects/js/js-challenge-two.js
@@ -0,0 +1,18 @@
+
+        //Excercise One :
+
+for (let i = 1; i <= 101; i++) {
+    if (i % 2 !== 0) console.log(i);
+}
+
+            //Excercise Two :
+
+            function showMessage() {
+                let message = 'Hello, ' + firstName + lastName;
+                alert(message);
+              }
+            let firstName = 'Jeffrey';
+            let lastName = 'Rios';
+            alert('Hello', firstName + lastName);
+            
+  
